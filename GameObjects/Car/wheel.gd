bbf1546b32770fe5d2a0dd249ee3f4d385a6ba95
Myles Rademaker <mylesprademaker@gmail.com>
2024-06-19 15:37:29 -0700

    Added steering input and lateral force to turn the car

--- GameObjects/Car/wheel.gd
+++ GameObjects/Car/wheel.gd
@@ -15,8 +15,40 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if is_colliding():
 		_suspension(delta, get_collision_point())
+		_apply_lateral_force(delta, get_collision_point())
 		if !is_front_wheel:
 			_acceleration(get_collision_point())
+
+
+func _apply_resistance(collision_point: Vector3) -> void:
+	var direction := global_basis.z
+	var wheel_velocity := _get_point_velcocity(global_position)
+	var resistance = direction.dot(wheel_velocity) * car.mass / car.drag
+	
+	car.apply_force(-direction * resistance, collision_point - car.global_position)
+	var point := Vector3(collision_point.x, collision_point.y + car.wheel_radius, collision_point.z)
+	
+	if car.debug:
+		DebugDraw3D.draw_arrow(point, point + (-direction * resistance * 2), Color.YELLOW, 0.1, true)
+
+
+func _apply_lateral_force(delta: float, collision_point: Vector3) -> void:
+	var direction := global_basis.x
+	var wheel_velocity := _get_point_velcocity(global_position)
+	var later_velocity := direction.dot(wheel_velocity)
+	
+	var grip := car.rear_wheel_grip
+	
+	if is_front_wheel:
+		grip = car.front_wheel_grip
+	
+	var desired_velocity_change := -later_velocity * grip
+	var lateral_force = desired_velocity_change + delta
+	
+	car.apply_force(direction * lateral_force, collision_point - car.global_position)
+	
+	if car.debug:
+		DebugDraw3D.draw_arrow(global_position, global_position + (direction * lateral_force), Color.PURPLE, 0.1, true)
 
 
 func _acceleration(collision_point: Vector3) -> void:
@@ -25,10 +57,14 @@ func _acceleration(collision_point: Vector3) -> void:
 	var torque = car.accel_input * car.engine_power
 	var point := Vector3(collision_point.x, collision_point.y + car.wheel_radius, collision_point.z)
 	
-	car.apply_force(accel_direction * torque, point - car.global_position)
+	if torque:
+		car.apply_force(accel_direction * torque, point - car.global_position)
+	else :
+		_apply_resistance(collision_point)
 	
 	if car.debug:
 		DebugDraw3D.draw_arrow(point, point + (accel_direction * torque), Color.BLUE, 0.1, true)
+
 
 func _suspension(delta: float, collision_point: Vector3) -> void:
 	var suspension_direction: Vector3 = global_basis.y
@@ -54,3 +90,7 @@ func _suspension(delta: float, collision_point: Vector3) -> void:
 		#DebugDraw3D.draw_sphere(point, 0.1)
 		DebugDraw3D.draw_arrow(global_position, to_global(position + Vector3(-position.x, (suspension_force.y / 2), -position.z)), Color.GREEN, 0.1, true)
 		DebugDraw3D.draw_line_hit_offset(global_position, to_global(position + Vector3(-position.x, -1, -position.z)), true, distance, 0.2, Color.RED, Color.RED)
+
+
+func _get_point_velcocity(point: Vector3) -> Vector3:
+	return car.linear_velocity + car.angular_velocity.cross(point - car.global_position)
